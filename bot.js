4892181a95a490049fc9bd01489f630cc4291db6
Mikhail Melamed <misha.melamed@hotmail.com>
2019-12-01 02:24:48 +0200

    Initial commit

---
+++ bot.js
@@ -0,0 +1,41 @@
+const Telegraf = require('telegraf')
+const asciimath2latex = require('asciimath-to-latex')
+
+const bot = new Telegraf('881556390:AAHJQCZoHDJMOlJjtirghnZByzUCWY24MPM')
+
+function getFormulaUrl(text) {
+    console.log(text)
+    const latex = asciimath2latex(text)
+    const uri = encodeURIComponent(latex)
+    const url = `https://math.now.sh?inline=${uri}.png`
+    return url
+}
+
+bot.start((ctx) => ctx.replyWithMarkdown('Welcome to LaTeX bot by [@SalmonMoses](tg://user?id=149477622)! You can check LaTeX documentation [here](https://www.latex-project.org/help/documentation/) and ASCIIMath [here](http://asciimath.org/)'))
+
+bot.command('math', ctx => {
+    let formula = ctx.message.text.split(' ')
+    formula.shift()
+    formula = formula.join('')
+    const url = getFormulaUrl(formula)
+    ctx.replyWithPhoto(url)
+})
+
+bot.on('text', ctx => {
+    if (ctx.message.chat.type !== 'private') return
+    const url = getFormulaUrl(ctx.message.text)
+    ctx.replyWithPhoto(url)
+})
+
+bot.on('inline_query', ctx => {
+    const url = getFormulaUrl(ctx.message.text)
+    ctx.answerInlineQuery([{
+        type: 'photo',
+        id: 1,
+        photo_url: url
+    }])
+})
+
+console.log("Started!")
+
+bot.launch()
