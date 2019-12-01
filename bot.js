29b11914181768f31b1eaeb36066cc8d57f32f5e
Mikhail Melamed <misha.melamed@hotmail.com>
2019-12-01 13:46:25 +0200

    Added logging

--- bot.js
+++ bot.js
@@ -1,19 +1,25 @@
-const {Composer} = require('micro-bot')
+const { Composer } = require('micro-bot')
 const asciimath2latex = require('asciimath-to-latex')
+const logger = require('pino')({
+    prettyPrint: true
+})
 
 const bot = new Composer()
 
 function getFormulaUrl(text) {
-    console.log(text)
     const latex = asciimath2latex(text)
     const uri = encodeURIComponent(latex)
     const url = `https://math.now.sh?inline=${uri}.png`
     return url
 }
 
-bot.start((ctx) => ctx.replyWithMarkdown('Welcome to LaTeX bot by [@SalmonMoses](tg://user?id=149477622)! You can check LaTeX documentation [here](https://www.latex-project.org/help/documentation/) and ASCIIMath [here](http://asciimath.org/)'))
+bot.start(ctx => {
+    logger.info(`${ctx.from.username} started using the bot!`)
+    ctx.replyWithMarkdown('Welcome to LaTeX bot by [@SalmonMoses](tg://user?id=149477622)! You can check LaTeX documentation [here](https://www.latex-project.org/help/documentation/) and ASCIIMath [here](http://asciimath.org/)')
+})
 
 bot.command('math', ctx => {
+    logger.info(`${ctx.from.username}: ${ctx.message.text}`)
     let formula = ctx.message.text.split(' ')
     formula.shift()
     formula = formula.join('')
@@ -22,6 +28,7 @@ bot.command('math', ctx => {
 })
 
 bot.on('text', ctx => {
+    logger.info(`${ctx.from.username}: ${ctx.message.text}`)
     if (ctx.message.chat.type !== 'private') return
     const url = getFormulaUrl(ctx.message.text)
     ctx.replyWithPhoto(url)
